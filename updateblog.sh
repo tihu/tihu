3294082865bab33346fc07844aa79f3677011d02
tihu <tihuke@gmail.com>
2020-01-16 18:41:26 +0800

    Updated source files

---
+++ updateblog.sh
@@ -0,0 +1,14 @@
+cd ~
+cd tihuke
+hugo
+git pull git@github.com:tihu/tihu-hugo-blog.git master
+git add .
+git commit -m ' Updated source files'
+git push origin master 
+
+cd public
+git pull git@github.com:tihu/tihublog.github.io.git master
+git add .
+git commit -m 'Update a blog'
+git push origin master
+date=$(date +%Y%m%d%H%M)
